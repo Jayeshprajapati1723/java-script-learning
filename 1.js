36ab47c1285c03d51abb4c780ef78d19188d574a
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-03-01 17:07:55 +0530

    first class basic of java script

---
+++ 1.js
@@ -0,0 +1,56 @@
+// this is my journey of java script 
+// so we start with very basics
+console.log ("github") ;
+
+//VARIABLE
+// name = "jayesh" ;
+num = 55 ;
+// age = 123 ;
+bool = true ;
+
+// variable declared with let
+let name = "jayesh PERFECT" ;
+// var 
+var names= "old way becouse it is redeclared of variables xxxx";
+// CONST
+const god= "ram" ;
+
+// let and const use only
+
+//class 2 data type
+//------------data type-----------------------------
+// 7 premitive 
+// number / string / boolean / undefined / bigint /symbol /null 
+let number = "jay" ;
+console.log (typeof number) ; //string 
+
+// non - premitive data type 
+// object 
+const student ={
+    name : "jayesh" ,
+    age : 21 ,
+    isStudent : 1 
+}
+// ways of print the object
+console.log (student.name) ;
+console.log (student[name]) ;
+console.log (student.age) ;
+console.log (student.isStudent) ;
+
+// update and reasign
+student["age"] = student ["age"] +1 ;
+console.log(student["age"]) ;
+
+// practice 1
+const product = {
+    title : "pen" , Mrp : 20 , sp : 18 , discout : 2 , isdeal : true , rating : "****/5*" 
+}
+const instaprofile = {
+    userid: "jayesh123" ,
+username :"jayesh prajapati",
+bio : "now a perfect bio" ,
+followers : 500 , following : 499 , posts : 50 , isfollow : true 
+ }
+
+ console.log( student ) ;
+ console.log (instaprofile) ; console.log(product) ;
